e0d8774f566612330e015a9dfa75e2fd412b1caa
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-10-04 20:20:33 +0700

    Initial Commit

---
+++ app/src/main/java/men/ngopi/aviedb/android_java_md_boilerplate/MainActivity.java
@@ -0,0 +1,13 @@
+package men.ngopi.aviedb.android_java_md_boilerplate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
